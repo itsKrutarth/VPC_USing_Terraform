ad1e5db7c7854cd222675347fd1cb75399ac0146
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-07-14 16:10:27 -0400

    declaring all vars we will use to create vpc

--- variables.tf
+++ variables.tf
@@ -4,3 +4,29 @@ variable "aws_region" {
   type = string
   default = "us-east-2"
 }
+
+variable "environment" {
+  description = "Environment name used in resource names and tags"
+  type = string
+  default = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "overall ip address range"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "tags" {
+  description = "Global tags to apply to all resources"
+  type = map(string)
+  default = {
+    "Terraform" = "true"
+  }
+}
+
+variable "subnet_newbits" {
+  description = "Numer of new bits we need to add to VPC CIDR to generate subnets"
+  type = number
+  default = 8
+}
