d3c0206afe4c20873f51ff7e23cc1be4c4e6be20
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-07-14 19:23:28 -0400

    createing resources

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+
+resource "aws_vpc" "vpc_tf_dev" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = merge(var.tags, {name = "vpc_tf_dev", owner = "Krutarth Patel"})
+#   lifecycle {
+#     prevent_destroy = true
+#   }
+}
+
+resource "aws_internet_gateway" "tf_igw" {
+  vpc_id = aws_vpc.vpc_tf_dev.id
+  tags = merge(vars.tags, {Name = "tf_igw", owner = "Krutarth Patel"})
+}
+
+resource "aws_subnet" "public_subnet_tf" {
+  vpc_id = aws_vpc.vpc_tf_dev.id
+  cidr_block = local.pubic_subnets
+  tags = merge(vars.tags, {Name = "tf_pub_sunet", owner = "Krutarth Patel"})
+}
