bef5ecda154eaa2b736e44e05e605a23932ac6bb
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-07-14 21:12:39 -0400

    outputs

--- output.tf
+++ output.tf
@@ -1,13 +1,21 @@
 
+output "aws_vpc" {
+  value = aws_vpc.vpc_tf_dev
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc_tf_dev.id
+}
+
 
 output "azs" {
   value = [for x in local.azs : x]
 }
 
 output "pub_subnets" {
-  value = [for x in local.public_subnets : x]
+  value = {for x, id in local.public_subnets : x => local.public_subnets.id}
 }
 
 output "private_subnets" {
-  value = [for x in local.private_subnets : x]
+  value = {for x, id in local.private_subnets : x => local.private_subnets.id}
 }
