1bf144ec2601b37a3fa254e0000f6b67545eaf1c
Krutarth Patel <mail2krutarthpatel@gmail.com>
2026-07-14 21:19:12 -0400

    output syntax

--- output.tf
+++ output.tf
@@ -13,9 +13,9 @@ output "public_subnets_map" {
 }
 
 output "pub_subnets" {
-  value = {for x, id in local.public_subnets : x => aws_subnet.public_tf.value.id}
+  value = {for x, id in local.public_subnets : x => aws_subnet.public_tf[each.value].id}
 }
 
 output "private_subnets" {
-  value = {for x, id in local.private_subnets : x => aws_subnet.private_tf.value.id}
+  value = {for x, id in local.private_subnets : x => aws_subnet.private_tf[each.value].id}
 }
